78fdb710d6d8044e526250ab84f6edba3da7ea10
KANOUK1136 <charly.fonb@gmail.com>
2024-01-16 09:51:00 +0100

    first ex1 all functionnalities

--- exo1.py
+++ exo1.py
@@ -3,11 +3,49 @@ import time
 class Robot():
     __name = "<unnamed>"
     __power = False
+    __action = ''
+    __speed = 0
     __current_speed = 0
     __battery_level = 0
     __states = ['shutown', 'running']
     
-        
+    #name
+    @property
+    def name(self):
+      return self.__name
+    
+    @name.setter
+    def name(self, name):
+      self.__name = name
+    
+    #power
+    @property
+    def power(self):
+      return self.__power
+    
+    @power.setter
+    def power(self, power):
+      self.__power = power
+    
+    #battery
+    @property
+    def battery_level(self):
+      return self.__battery_level
+    
+    @battery_level.setter
+    def battery_level(self, battery_level):
+      self.__battery_level = battery_level
+
+    #action
+    @property
+    def action(self):
+      return self.__action
+    
+    @power.setter
+    def action(self, action):
+      if self.__power:
+        self.__action = action
+
     """
       Give your best code here ( •̀ ω •́ )✧
     """
@@ -18,22 +56,65 @@ class Robot():
         self.__name = name
       self.__current_state = self.__states[0]
       self.__power = False
+      self.__speed = 0
 
     def boot(self):
-      self.__power = True
-      self.__current_state = self.__states[1]
+      if self.__power == False:
+        self.__power = True
+        self.__current_state = self.__states[1]
+        print("ON")
       pass
 	
     def shutdown(self):
-      self.__power = False
-      self.__current_state = self.__states[0]
+      if self.__power == True:
+        self.__power = False
+        self.__speed = 0
+        print("OFF")
       pass
 	
     def charge(self):
       for i in range(self.__battery_level,100):
         self.__battery_level = self.__battery_level + 1
-        print("charge de la batterie : %d", self.__battery_level)
-        time.sleep(0.1) 	
-      pass
+        print("charge de la batterie : ", self.__battery_level,"%")
+        time.sleep(0.1) 
+      return self.__battery_level
 
+    def move(self):
+      if self.__power == True:
+        self.__speed = 10
+        print("vitesse actuelle : ", self.__speed)
+      else:
+        print(r1.__name, "is off")
     
+    def stop(self):
+      if self.__power == True:
+        self.__speed = 0
+        print("vitesse actuelle : ", self.__speed)
+      else:
+        print(r1.__name, "is off")
+    
+    def check(self):
+      print("""
+system state display
+--------------------
+""")
+      print("name :", self.__name)
+      print("battery :", self.__battery_level)
+      print("power :", self.__power)
+      print("speed :", self.__speed)
+
+if __name__ == '__main__':
+  r1 = Robot()
+  r1.name = "feur"
+
+  print(r1.battery_level)
+  r1.boot()
+  r1.charge()
+  print(r1.battery_level)
+  r1.move()
+  r1.stop()
+  r1.shutdown()
+  r1.check()
+  
+  
+
