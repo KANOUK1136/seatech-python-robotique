4a1de41fbbb561936d69488fa0f8c22e47ada2dd
KANOUK1136 <charly.fonb@gmail.com>
2024-01-23 08:35:27 +0100

    added method fun

--- exo2.py
+++ exo2.py
@@ -1,4 +1,6 @@
+import time
 from exo1 import Robot
+import random
 
 class Human():
     __sexe = ['F','M']
@@ -32,35 +34,48 @@ class Human():
     """Human class content here"""
     def __init__(self, sexe):
         self.__sexe = sexe
+        
 
     def add_aliment(self):
-       temp = input('entrez aliment au menu :')
-       self.__list_aliment.append(temp)
+        temp = input('entrez aliment au menu :')
+        self.__list_aliment.append(temp)
+
 
     def eat(self):
-       for aliment in self.__list_aliment:
+        for aliment in self.__list_aliment:
           print("eating", aliment)
+
 
     def digest(self):
         self.__list_aliment = []
         print("digested")
 
 class Cyborg(Robot, Human):   
-
     def __init__(self, name, sexe):   
         Robot.__init__(self, name)
         Human.__init__(self, sexe)
 
+    def cyberpsychosis(self):
+        a = random.randrange(1,10)
+        if a == 5 or a == 6:
+            print("AHAHAHHAHAHH")
+            for i in range(0,20):
+                time.sleep(0.1) 
+                print("s̴̠̒͋̈́̔p̶͚̘̪̥̗̳͕̍̑̒̌̌̿̂̔̿̚͜͝é̶̥̻͉̜̱̝͓̰̻̠́͌͑̇̿̂̾̓͊͜e̵̡̮̼̜̞̺͓͌͝d̵̙͇̗͕̽̀̃̓ ̶̨̧̪̰̥̲͔̳̰͇͙͓͌͆̈́̈̃̕ĭ̸̭͍̜̭̤͎̞̭̰̈́ş̴̧̥̳͉̹͙̓͗͛͂̓͊̃ ̸̡̛͉̮̖͕̯̥̥̘̣̫̬̾͋̾̀̔̓̌̐̀͂͂͘͝w̶̢͎̖͒͌̎̃̔̈̕ĥ̴̨̘͙͍̞͕̝̰̙̟̣̤́̿̐͠ͅā̷̢̪͍͎̻̼͓̥̗̌̎̔̎͐̀̃̈́̍͌͝t̶̨̡̺̻̺̥̮̹̘̮͖͍͖̏̈́̊̎'̵͍̺̾̊̐ş̶͚̖̦̥̯͚͔̬̱͙̦̞̣̩̄͐̈́̃͛̚͘͝ ̴̡̨̢͔̟̦̤̤̤̲͍̹̈̅̃̔͛̾͒̌̑̈̈́̇͝͠͠ÿ̷̡̤̳͇͙͍̼͙́̓̀̏̆̍̀̎̑̑͜o̶̳̖̮͕̲̼̘̭̪̲͍͈͗̀̉͊́̽͜͝͝͝u̸͍͎͙̳͔̬͐̉͑́͠'̶͙̿̈́̅͒̿̉̏̂́̌̕͝r̴̡͙͕̻̮͙̟̫̤͇̼̘̗͕͌͗̿̈͊͐͝e̸͍̳̳̰̺̳̥̮̦͈̦͊̿͒̀̂͆̽̌͘ ̴̥̠̩̦̱̻͆͜ḍ̶̡̦̩̻̓͑̾ͅơ̶̡̠̺͚̯͓̠̬̙͇̦̽͗͒i̶̛͕̻̤͍͈͕̩̐͆͗́̎̿̓͆̀̉̆̀͝͝ṋ̶̤͕͇̦̣̗̩̣͊͛g̸̢̧̯̦͓̬̣̩̱̲͓͓̫̤͊̍͂͒ ̴̧͔̝̗̻͕͉͚͔̜͚͌̅̾̓̃̊̊̇̆̈͘͘̕b̶̡̡̬̺̘̻͚̺̺͑͊̍̒͊̈́̓̃̃ȅ̷͊̄̂ͅs̸̤̻̘̩͇͎̺͕̠͍͉͇͗̅̈̿̾̐̈́̔̈́̚͘͝ͅt̶̢͚͎͚̪̬̝̪͇͈̑̽͂͜ ̴̠̘̯̬̯̱̘̞͙̬͔̎̋̈́?̵̨̛̯͙̰̦̝͇͓̱̯̆̋̍͒̑̑̉̌̃̂͂͘͠͠ͅ")
+            print("human part killed")    
+            exit()
 
 cyborg = Cyborg('Sandevistan', 'M')
 
 print(cyborg.name, 'sexe', cyborg.sexe)
 print('Charging battery...')
 cyborg.charge()
+cyborg.cyberpsychosis()
 cyborg.check()
+cyborg.cyberpsychosis()
 cyborg.add_aliment()
+cyborg.cyberpsychosis()
 cyborg.eat()
+cyborg.cyberpsychosis()
 cyborg.digest()
-print("sexe :", cyborg.sexe)
-
-# cyborg.truc_fun()
+cyborg.cyberpsychosis()
